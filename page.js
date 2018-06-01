20a6a18ac0d24511c80692489b1267e476b4c2ab
Mas4erLomas4er <i9997117@gmail.com>
2018-06-01 11:48:06 +0300

    c

--- page.js
+++ page.js
@@ -1,22 +1,31 @@
 window.onload = function () {
+	let mainBlock = document.querySelector('.slider-block');
+	let mainBlockTop = Math.floor(mainBlock.getBoundingClientRect().top + window.pageYOffset);
 	let checkboxes = document.querySelectorAll('.check');
-	let labels = this.document.querySelectorAll('.slider-block label');
+	let labels = document.querySelectorAll('.slider-block label');
 	let i = 0;
-	let timeFunc = setInterval(function () {
-		for (let j = 0; j < labels.length; j++) {
-			labels[j].onclick = function () {
-				checkboxes[j].setAttribute('checked', 'true');
-				clearInterval(timeFunc);
-			}
-		}
-		checkboxes[i].removeAttribute('checked');
-		i++;
-		if (i > 0 && i < 10) {
-			i = i;
+
+	window.onscroll = function () {
+		if ((window.pageYOffset + window.innerHeight) < mainBlockTop) {
+			console.log('l');
 		} else {
-			i = 0;
+			let timeFunc = setInterval(function () {
+				for (let j = 0; j < labels.length; j++) {
+					labels[j].onclick = function () {
+						checkboxes[j].setAttribute('checked', 'true');
+						clearInterval(timeFunc);
+					}
+				}
+				checkboxes[i].removeAttribute('checked');
+				i++;
+				if (i > 0 && i < 10) {
+					i = i;
+				} else {
+					i = 0;
+				}
+				checkboxes[i].setAttribute('checked', 'true');
+			}, 10000);
+			window.onscroll = true;
 		}
-		checkboxes[i].setAttribute('checked', 'true');
-		console.log(checkboxes[i]);
-	}, 10000);
+	}
 }
